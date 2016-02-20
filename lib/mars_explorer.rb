e3d2aa0941334582fd4499bf927d06e94e5a62b7
Fotos Georgiadis <gfotos@gmail.com>
2016-02-20 19:09:41 +0100

    Add MarsExplorer.run

--- lib/mars_explorer.rb
+++ lib/mars_explorer.rb
@@ -4,5 +4,36 @@ require 'mars_explorer/plateau'
 require 'mars_explorer/rover'
 
 module MarsExplorer
-  # Your code goes here...
+  class << self
+    def run(io)
+      plateau = create_plateau(io.gets.chomp!)
+
+      until io.eof?
+        rover = create_rover(plateau, io.gets.chomp!)
+        instructions = parse_instructions(io.gets.chomp!)
+
+        rover.follow_instructions!(instructions)
+
+        puts rover
+      end
+    end
+
+    private
+
+    def create_plateau(line)
+      x, y = line.split(' ')
+
+      Plateau.new(x: Dimension.new(max: x), y: Dimension.new(max: y))
+    end
+
+    def create_rover(plateau, line)
+      lng, lat, heading = line.split(' ')
+
+      Rover.new(plateau: plateau, lng: lng, lat: lat, heading: heading)
+    end
+
+    def parse_instructions(line)
+      line.split('')
+    end
+  end
 end
--- spec/mars_explorer_spec.rb
+++ spec/mars_explorer_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
+require 'yaml'
 
 describe MarsExplorer do
   it 'has a version number' do
     expect(MarsExplorer::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.run' do
+    Dir[File.join('spec', 'fixtures', '**', 'scenario_*.yml')].each do |file|
+      context "with scenario #{file}" do
+        let(:scenario) { YAML.load_file(file)[:scenario] }
+        let(:input) { StringIO.new(scenario[:input]) }
+
+        specify do
+          expect { described_class.run(input) }.to output(scenario[:output]).to_stdout
+        end
+      end
+    end
   end
 end
