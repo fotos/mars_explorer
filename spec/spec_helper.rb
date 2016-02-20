4f0ca6fe2b2acc894ef211dd93bf9183ff2b6bcb
Fotos Georgiadis <gfotos@gmail.com>
2016-02-20 16:55:51 +0100

    Setup factory_girl in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mars_explorer'
+
+require 'factory_girl'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    FactoryGirl.find_definitions
+  end
+end
